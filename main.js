76c85e2f4781e3c9c343f5b9ec1a23c6369d20dd
sumaiyaibrahim04 <125154848+sumaiyaibrahim04@users.noreply.github.com>
2023-04-08 20:06:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,85 +1,60 @@
-const todos = [];
+//Getting all required elements
+const inputField = document.querySelector(".input-field textarea"),
+  todoLists = document.querySelector(".todoLists"),
+  pendingNum = document.querySelector(".pending-num"),
+  clearButton = document.querySelector(".clear-button");
 
-let checkBox = document.createElement('input');
+//we will call this function while adding, deleting and checking-unchecking the task
+function allTasks() {
+  let tasks = document.querySelectorAll(".pending");
 
+  //if tasks' length is 0 then pending num text content will be no, if not then pending num value will be task's length
+  pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
 
-const todoList = document.querySelector(".todo-list");
-const form = document.querySelector(".add-todo-form");
-const toDoInputBox = document.querySelector("#todo-name");
-const deleteAllButton = document.querySelector(".delete-all");
-
-todoList.classList.add("className");
-
-console.log(todoList);
-
-function renderToDoList() {
-  // Clear all of the entries in the list
-  while (todoList.firstChild) {
-    todoList.removeChild(todoList.firstChild);
-  }
-
-  for (let i = 0; i < todos.length; i++) {
-    const todoListItem = document.createElement("li");
-    
-    todoListItem.textContent = todos[i].text+" " ;
-    /*const todoListCheck = document.createElement("button");
-    todoListCheck.textContent = "v";*/
-
-    const todoListButton = document.createElement("button");
-    todoListButton.textContent = "x";
-
-    todoListButton.dataset.index = i;
-
-    todoListItem.appendChild(todoListButton);
-
-    todoList.appendChild(todoListItem);
-  }
-}
-
-function addTodoItem(event) {
-  event.preventDefault();
-
-  const newTodo = toDoInputBox.value;
-
-  todos.push({
-    text: newTodo,
-    isDone: false,
-  });
-
-  console.log("I AM A FUNCTION", newTodo);
-
-  console.log(todos);
-
-  renderToDoList();
-}
-
-function handleButtonClickInsideUl(event) {
-  if (event.target.nodeName !== "BUTTON") {
+  let allLists = document.querySelectorAll(".list");
+  if (allLists.length > 0) {
+    todoLists.style.marginTop = "20px";
+    clearButton.style.pointerEvents = "auto";
     return;
   }
-
-  todoArrayIndexToDelete = event.target.dataset.index;
-
-  todos.splice(todoArrayIndexToDelete, 1);
-
-  console.log(todos);
-
-  renderToDoList();
+  todoLists.style.marginTop = "0px";
+  clearButton.style.pointerEvents = "none";
 }
 
-form.addEventListener("submit", addTodoItem);
-todoList.addEventListener("click", handleButtonClickInsideUl);
+//add task while we put value in text area and press enter
+inputField.addEventListener("keyup", (e) => {
+  let inputVal = inputField.value.trim(); //trim fuction removes space of front and back of the inputed value
 
-renderToDoList();
+  //if enter button is clicked and inputed value length is greated than 0.
+  if (e.key === "Enter" && inputVal.length > 0) {
+    let liTag = ` <li class="list pending" onclick="handleStatus(this)">
+          <input type="checkbox" />
+          <span class="task">${inputVal}</span>
+          <i class="fa fa-close" onclick="deleteTask(this)"></i>
+        </li>`;
 
-function deleteAllTodos(event) {
-  todos.length = 0;
+    todoLists.insertAdjacentHTML("beforeend", liTag); //inserting li tag inside the todolist div
+    inputField.value = ""; //removing value from input field
+    allTasks();
+  }
+});
 
-  renderToDoList();
+//checking and unchecking the chekbox while we click on the task
+function handleStatus(e) {
+  const checkbox = e.querySelector("input"); //getting checkbox
+  checkbox.checked = checkbox.checked ? false : true;
+  e.classList.toggle("pending");
+  allTasks();
 }
 
-/*if (todos.length==0){
-  ul.innerHTML = ''
-}*/
+//deleting task while we click on the delete icon.
+function deleteTask(e) {
+  e.parentElement.remove(); //getting parent element and remove it
+  allTasks();
+}
 
-deleteAllButton.addEventListener("click", deleteAllTodos);
+//deleting all the tasks while we click on the clear button.
+clearButton.addEventListener("click", () => {
+  todoLists.innerHTML = "";
+  allTasks();
+});
